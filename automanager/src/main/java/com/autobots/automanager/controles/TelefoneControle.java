f028b4e301d2b5c08783677ff32eae7e7483f5e7
Giovana O. Silva <78885451+gioliveirass@users.noreply.github.com>
2022-03-15 08:27:36 -0300

    feat: get telefone by id

---
+++ automanager/src/main/java/com/autobots/automanager/controles/TelefoneControle.java
@@ -0,0 +1,34 @@
+package com.autobots.automanager.controles;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.autobots.automanager.entidades.Telefone;
+import com.autobots.automanager.modelo.ClienteSelecionador;
+import com.autobots.automanager.modelo.TelefoneSelecionador;
+import com.autobots.automanager.repositorios.ClienteRepositorio;
+import com.autobots.automanager.repositorios.TelefoneRepositorio;
+
+@RestController
+@RequestMapping("/telefone")
+public class TelefoneControle {
+	@Autowired
+	private TelefoneRepositorio repositorio;
+	@Autowired
+	private TelefoneSelecionador selecionador;
+	@Autowired
+	private ClienteRepositorio repositorioCliente;
+	@Autowired
+	private ClienteSelecionador selecionadorCliente;
+	
+	@GetMapping("/telefone/{id}")
+	public Telefone obterTelefone(@PathVariable long id) {
+		List<Telefone> telefones = repositorio.findAll();
+		return selecionador.selecionar(telefones, id);
+	}
+}
